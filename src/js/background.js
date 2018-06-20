064ae605c9a9456423eb17e54b80d5483f30ded1
terryBaz <et_bazine@esi.dz>
2018-06-20 10:29:43 +0100

    small fix

--- src/js/background.js
+++ src/js/background.js
@@ -66,3 +66,38 @@ chrome.storage.local.get(['extensionIsOn'],
             path: '../icons/icon' + ((results.extensionIsOn == true) ? '' : '_off') + '.png'
         });
     });
+
+chrome.tabs.onUpdated.addListener(function (tabId,info,tab_) {
+
+    chrome.storage.local.get(['extensionIsOn'],
+        function (results) {
+
+            // Search
+            console.log("222ttt " +results.extensionIsOn);
+            if (results.extensionIsOn && tabId) {
+                chrome.tabs.executeScript(tabId, {file: "js/jquery-3.3.1.min.js"}, function () {
+                    chrome.tabs.executeScript(tabId, {file: "js/content.js"}, function () {
+                        chrome.tabs.executeScript(tabId, {file: "js/popup.js"}, function () {
+                            const lastErr = chrome.runtime.lastError;
+                            if (lastErr) console.log('tab: ' + tabId + ' lastError: ' + JSON.stringify(lastErr));
+                            // This executes only after your content script executes
+                            console.log("yessss");
+                            chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+                                chrome.tabs.sendMessage(tabId, {
+                                    'message': 'searchNt'
+                                })
+                            });
+                        });
+                    });
+                });
+            }
+
+
+            console.log(results);
+            chrome.browserAction.setIcon({
+                path: '../icons/icon' + ((results.extensionIsOn == true) ? '' : '_off') + '.png'
+            });
+        });
+
+
+})
